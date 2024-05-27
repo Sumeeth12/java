e254648539d47a46b06545f8963f11c86fe0608b
Sumeeth12 <chindamsumeeth5125@gmail.com>
2024-05-27 17:48:14 +0530

    Create GeometryCalculater.java

---
+++ java_pratice/GeometryCalculater.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = new int[3][4];
+        System.out.println("Enter the elements of the matrix (3x4):");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Enter element at position (" + (i + 1) + ", " + (j + 1) + "): ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("\nOriginal Matrix:");
+        printMatrix(matrix);
+        System.out.println("\nTranspose of the Matrix:");
+        int[][] transpose = transposeMatrix(matrix);
+        printMatrix(transpose);
+
+        scanner.close();
+    }
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+        return transpose;
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
